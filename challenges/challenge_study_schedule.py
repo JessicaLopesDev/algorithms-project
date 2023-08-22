90f338f9148c30361c11453a12fb359120d5842b
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-08-22 15:42:32 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None: return None
+
+    result = 0
+    for period in permanence_period:
+        if type(period[0]) != int or type(period[1]) != int:
+            return None
+        if period[1] >= target_time and period[0] <= target_time:
+            result += 1
+
+    return result
