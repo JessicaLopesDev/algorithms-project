142d50168f50303b3b56dc657b6279d6028cd432
Jessica Lopes <jessicalopes.dev@gmail.com>
2023-08-22 17:06:45 -0300

    fix lint

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,6 +1,7 @@
 from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
+
 def test_encrypt_message():
     with pytest.raises(TypeError, match="tipo inválido para key"):
         encrypt_message("jessica", "j")
@@ -11,4 +12,3 @@ def test_encrypt_message():
     assert encrypt_message("jessica", 2) == "aciss_ej"
     assert encrypt_message("jessica", 3) == "sej_acis"
     assert encrypt_message("jessica", 8) == "acissej"
-
